59742a9e7843dcfd3de5594adc1017cf9b9f87ea
ngeojiajun-deriv <jia.jun@deriv.com>
2023-06-16 13:04:52 +0800

    more visualizations and use field filter

--- srps/visualizations.metabase.sql
+++ srps/visualizations.metabase.sql
@@ -1,15 +1,15 @@
 -- 01-claim status by purchase_type
 select purchase_type,claim_status, COUNT(*) as "count"
     FROM "claims"."claims"
-    [[WHERE claim_date >= {{start_date}} AND claim_date <= {{end_date}} ]]
+    WHERE {{claim_date}}
     GROUP BY 1,2;
 
 -- 02-claim status by employees
 select employee_name,claim_status, COUNT(*) as "count"
-    FROM "claims"."claims" c
+    FROM "claims"."claims"
    INNER JOIN "claims"."employee"
    USING (employee_id)
-   [[WHERE claim_date >= {{start_date}} AND claim_date <= {{end_date}} ]]
+   WHERE {{claim_date}}
    GROUP BY 1,2;
 
 -- 03-claim status by vendor
@@ -23,5 +23,19 @@ SELECT
     FROM "claims"."claims" c
     INNER JOIN "claims"."vendor" v
     USING (vendor_id)
-    [[WHERE claim_date >= {{start_date}} AND claim_date <= {{end_date}} ]]
+    WHERE {{claim_date}}
     GROUP BY 1,2;
+
+-- 04-total claims by employee
+SELECT employee_name, SUM(price) as total_claim
+    FROM "claims"."claims" c
+   INNER JOIN "claims"."employee" e
+   USING (employee_id)
+   WHERE c.employee_id > 1 AND {{claim_date}}
+   GROUP BY 1;
+
+-- 05-claim status
+select claim_status, COUNT(*) as "count"
+    FROM "claims"."claims"
+    WHERE {{claim_date}}
+    GROUP BY 1;
