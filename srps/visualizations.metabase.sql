06466bfac32760f18d075eea3f98610d72eedf39
ngeojiajun-deriv <jia.jun@deriv.com>
2023-06-16 12:38:45 +0800

    add more visualizations

--- srps/visualizations.metabase.sql
+++ srps/visualizations.metabase.sql
@@ -11,3 +11,17 @@ select employee_name,claim_status, COUNT(*) as "count"
    USING (employee_id)
    [[WHERE claim_date >= {{start_date}} AND claim_date <= {{end_date}} ]]
    GROUP BY 1,2;
+
+-- 03-claim status by vendor
+SELECT
+    CASE
+        WHEN c.vendor_id = 1 THEN COALESCE(c.vendor_name, 'Unknown')
+        ELSE v.vendor_name
+    END as vendor_name,
+    claim_status,
+    COUNT(*) as "count"
+    FROM "claims"."claims" c
+    INNER JOIN "claims"."vendor" v
+    USING (vendor_id)
+    [[WHERE claim_date >= {{start_date}} AND claim_date <= {{end_date}} ]]
+    GROUP BY 1,2;
